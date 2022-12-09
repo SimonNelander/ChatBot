c47361528f6cdb629e8c562fd4f2e4230cb9bfc2
Simon Nelander <simon.nelander@learnet.se>
2022-12-09 14:48:02 +0100

    Lägger up WPF programmet

---
+++ chatBoten/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chatBoten
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HelpButton_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Navarrow.Visibility = Visibility.Visible;
+            hjalp.Visibility = Visibility.Visible;
+            HelpButton.Visibility = Visibility.Hidden;
+            HelpButton_Copy.Visibility = Visibility.Hidden;
+        }
+
+        private void homebutton_Click(object sender, RoutedEventArgs e)
+        {
+            HelpButton.Visibility = Visibility.Visible;
+            HelpButton_Copy.Visibility = Visibility.Visible;
+            hjalp.Visibility = Visibility.Hidden;
+            Navarrow.Visibility = Visibility.Hidden;
+        }
+    }
+}
